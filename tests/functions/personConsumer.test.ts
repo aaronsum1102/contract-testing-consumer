80b3a2ce6809591787cbaebab5b8b4ef4537d67f
aaronsum1102 <sumsx03@gmail.com>
2021-03-29 12:20:06 +0200

    Add consumer test

---
+++ tests/functions/personConsumer.test.ts
@@ -0,0 +1,47 @@
+import { processEvent } from "../../src/functions/personConsumer";
+import {
+  MessageConsumerPact,
+  synchronousBodyHandler,
+} from "@pact-foundation/pact";
+import { like } from "@pact-foundation/pact/src/dsl/matchers";
+import path from "path";
+
+describe("Consumer event handler test", () => {
+  const messagePact = new MessageConsumerPact({
+    consumer: "person-consumer",
+    dir: path.resolve(process.cwd(), "pacts"),
+    pactfileWriteMode: "update",
+    provider: "person-provider",
+    logLevel: "info",
+  });
+
+  it("Accep a valid person", async () => {
+    await expect(
+      messagePact
+        .expectsToReceive("A request for person")
+        .withContent({
+          firstName: like("Test"),
+          lastName: like("Testsson"),
+        })
+        .withMetadata({
+          "content-type": "application/json",
+        })
+        .verify(synchronousBodyHandler(processEvent))
+    ).resolves.not.toThrow();
+  });
+
+  it("Expect to throw error", async () => {
+    const expectedError = 'The parameter "lastName" is required.';
+    await expect(
+      messagePact
+        .expectsToReceive("A request for person")
+        .withContent({
+          firstName: like("Test"),
+        })
+        .withMetadata({
+          "content-type": "application/json",
+        })
+        .verify(synchronousBodyHandler(processEvent))
+    ).rejects.toThrow(expectedError);
+  });
+});
